7c5fe8c00304f7bf8181ff585143df4ac988286e
BinhLt <thanhbinhltbbn@gmail.com>
2018-09-29 23:38:30 +0700

    api_get

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,68 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react';
 
 const input100 = {
     color: 'red'
 }
 
 class Login extends React.Component {
-    constructor() {
-        super();
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePswChange = this.handlePswChange.bind(this);
-        this.state = {
-            emailField: '',
-            pswField: ''
-        };
-    }
 
-    submitHandler(evt) {
-        evt.preventDefault();
+  render() {
+    return (
+      <div className="limiter">
+            <div className="container-login100">
+                <div className="wrap-login100">
+                    <div className="login100-form validate-form">
 
-        this.setState({
-            emailField: '',
-            pswField: ''
-        });
+                        <span>{this.props.loginResult}</span>
 
-
-    }
-
-    handleEmailChange(event) { this.setState({ emailField: event.target.value }); }
-    handlePswChange(eve) { this.setState({ pswField: eve.target.value }); }
-
-    render() {
-        return (
-            <div className="limiter">
-                <div className="container-login100">
-                    <div className="wrap-login100">
-                        <form onSubmit={this.submitHandler} className="login100-form validate-form">
-                            <span className="login100-form-title p-b-34 m-b-20">
-                                LMS Login
+                        <span className="login100-form-title p-b-34 m-b-20">
+                            LMS Login
                         </span>
-                            <p>Email:</p>
-                            <div className="wrap-input100 m-b-20" data-validate="Type user name">
-                                <input onChange={this.handleEmailChange} id="first" className="input100" style={input100}
-                                    placeholder="User name" value={this.state.emailField} type="text" />
+                        <p>Email:</p>
+                        <div className="wrap-input100 m-b-20 validate-input m-b-20" data-validate="Type user name">
+                            <input onChange={this.props.onEmailChange} id="first" className="input100"
+                             name="username" placeholder="User name" value={this.props.email} style={input100}/>
+                        </div>
+                        <p>Password:</p>
+                        <div className="wrap-input100 m-b-20" data-validate="Type password">
+                            <input onChange={this.props.onPasswordChange} value={this.props.password} id="psw"
+                            className="input100" type="password" name="pass" placeholder="Password" style={input100}/>
+                        </div>
 
-                            </div>
-                            <p>Password:</p>
-                            <div className="wrap-input100 m-b-20" data-validate="Type user name">
-                                <input onChange={this.handlePswChange} id="first1" className="input100" style={input100}
-                                    placeholder="Password" value={this.state.pswField} type="password" />
-
-                            </div>
-
-                            <div className="container-login100-form-btn">
-                                <Link to="/homepage">
-                                    <button type="submit" className="login100-form-btn">
-                                        Sign in
-                            </button></Link>
-                            </div>
-
-                        </form>
-
-                        <div className="login100-more" style={{ backgroundImage: 'url("images/bg-01.jpg")' }}></div>
+                        <div className="container-login100-form-btn">
+                        <button className="login100-form-btn" onClick={this.props.submitClick}>
+                                Sign in
+                            </button>
+                        </div>
+                    </div>
+                    <div className="login100-more" style={{ backgroundImage: 'url("images/bg-01.jpg")' }}></div>
                     </div>
                 </div>
             </div>
