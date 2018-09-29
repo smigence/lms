3ef9d7522d51791624ed10f7d4a9f23229dc4d60
Trinhgiang520 <trinhgiang520@gmail.com>
2018-09-29 23:08:26 +0700

    dbadd

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,20 +6,20 @@ var con =   mysql.createConnection ({
   host: "localhost",
   user: "root",
   password: "",
-  database: "react_app"
+  database: "lmssystem"
 });
 
 var router = express.Router();
 router.use(cors());
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM account", function (err, result, fields) {
-    if (err) throw err;
-    router.get('/', function(req, res, next) {
-      res.json(result);
-    });
-  });
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM account", function (err, result, fields) {
+//     if (err) throw err;
+//     router.get('/', function(req, res, next) {
+//       res.json(result);
+//     });
+//   });
+// });
 
 module.exports = router;
